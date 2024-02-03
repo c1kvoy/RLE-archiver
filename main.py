051321da07d434ff5b50dee351b0478ed768cada
C1kvoy <seva33039@gmail.com>
2024-02-03 14:07:42 +0300

    Дописал функцию декодинга RLE текстовой строки modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #today we are write RLE encoder
+def txtdecode(s):
+    output = ""
+    i = 0
+    while i < len(s)-1:
+        if s[i+1].isdigit():
+            count = int(s[i+1])
+            output += count * s[i]
+            i += 2
+        else:
+            output += s[i]
+            i += 1
+    return output
 def txtencode(s):
     encoded =""
     count=1
-    for i in range(1,len(s)):
+    for i in range(0,len(s)-1):
         if s[i]==s[i-1]:
             count+=1
         else:
@@ -10,6 +22,8 @@ def txtencode(s):
             count=1
     encoded += s[-1]+str(count)
     return encoded
-s = "aaabbbgggGGG"
+s = "aaaaaakkkkk"
 s = txtencode(s)
 print(s)
+decodestring = txtdecode(s)
+print(decodestring)
