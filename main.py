6daee9a75b9169e01c23e010dc7b7c16d1d7def9
C1kvoy <seva33039@gmail.com>
2024-02-13 19:16:26 +0300

    Исправлена ошибка с одиночным символом в конце и начале строки, полность завершена работа с функциями работающими с текстом

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-#today we are write RLE encoder
 def txtdecode(s):
     output = ""
     i = 0
-    while i < len(s)-1:
-        if s[i+1].isdigit():
-            count = int(s[i+1])
+    while i < len(s):
+        if i < len(s) - 1 and s[i + 1].isdigit():
+            count = int(s[i + 1])
             output += count * s[i]
             i += 2
         else:
             output += s[i]
             i += 1
     return output
+
 def txtencode(s):
-    encoded =""
-    count=1
-    for i in range(0,len(s)-1):
-        if s[i]==s[i-1]:
-            count+=1
+    encoded = ""
+    count = 1
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            count += 1
         else:
-            encoded += s[i-1]+str(count)
-            count=1
-    encoded += s[-1]+str(count)
+            if count > 1:
+                encoded += s[i] + str(count)
+            else:
+                encoded += s[i]
+            count = 1
+    if count > 1:
+        encoded += s[-1] + str(count)
+    else:
+        encoded += s[-1]
     return encoded
-s = "aaaaaakkkkk"
-s = txtencode(s)
-print(s)
-decodestring = txtdecode(s)
-print(decodestring)
+
+s = input("Введите строку для кодирования: ")
+encoded_string = txtencode(s)
+print("Закодированная строка:", encoded_string)
+decoded_string = txtdecode(encoded_string)
+print("Декодированная строка:", decoded_string)
