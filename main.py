7772809d1cdcc2241e34e5445b4acbbad3002f97
C1kvoy <seva33039@gmail.com>
2024-05-11 21:34:05 +0700

    Полная переработка функция инкодирования и декодирования.добавление возможность сжимать картинки с 16кк цветов

--- main.py
+++ main.py
@@ -1,53 +1,77 @@
-
 import customtkinter
 from customtkinter import filedialog
 from PIL import Image
 # Функция для декодирования строки
-def txtdecode(encoded_str):
-    decoded_str = ''
+def bmp_encode(pixels):
+    result = ""
+    for row in pixels:
+        for i in range(3):
+            result += f"{row[i]:02X}"
+        result += " "
+    results = result[:-1]
+    results = results.split(' ')
+    count = 1
+    flag = 1
+    encoded_string = ""
+    for i in range(len(results)-1):
+        if results[i] == results[i+1]:
+            count+=1
+        else:
+            encoded_string+=str(count)+'x'+results[i]+' '
+            flag = 0
+            count = 1
+    encoded_string += str(count) + 'x' + results[-1]
+    return encoded_string
+
+def bmp_decode(encoded_string):
+    colors = []
+    encoded_string = encoded_string.split(' ')
+    for i in range(len(encoded_string)):
+        count = ""
+        for j in range(len(encoded_string[i])):
+            if encoded_string[i][j]!='x':
+                count += encoded_string[i][j]
+            else:
+                break
+        encoded_string[i] = encoded_string[i][len(count)+1:]
+        t = 1
+        for t in range(int(count)):
+            decimal_value = int(encoded_string[i], 16)
+            r = decimal_value >> 16
+            g = (decimal_value >> 8) & 0xFF
+            b = decimal_value & 0xFF
+            colors.append((r, g, b))
+    return colors
+def txtdecode(s):
+    decoded_str = ""
     i = 0
-    while i < len(encoded_str):
-        char = encoded_str[i]
-        while (i + 1 < len(encoded_str) and encoded_str[i + 1] != "⁊" or i == len(encoded_str)):
-            decoded_str += char
-            i += 1
-            char = encoded_str[i]
-        i += 2
+    while i < len(s):
+        char = s[i]
+        j = i + 2
         count = ''
-        while i < len(encoded_str) and encoded_str[i].isdigit():
-            count += encoded_str[i]
-            i += 1
-        if count:
-            decoded_str += char * int(count)
-        if i < len(encoded_str) and encoded_str[i] == '⁊':
-            i += 1
-    decoded_str+=char
+        while s[j] != ' ':
+            count += s[j]
+            j += 1
+        decoded_str += char * int(count)
+        i = j + 1
     return decoded_str
 
 # Функция для кодирования строки
 def txtencode(s):
-    encoded = ""
-    count = 1
-    for i in range(len(s) - 1):
-        if s[i] == s[i + 1]:
+    i, count = 1, 1
+    encoded_str = ""
+
+    while i < len(s):
+        if s[i] == s[i - 1]:
             count += 1
         else:
-            if count > 1:
-                if s[i].isdigit():
-                    encoded += s[i] + "⁊" + str(count) + "⁊"
-                else:
-                    encoded += s[i] + str(count)
-            else:
-                encoded += s[i]
+            encoded_str += s[i - 1] + " " + str(count) + " "
             count = 1
-    if count > 1:
-        if s[-1].isdigit():
-            encoded +=s[-1] + "⁊" + str(count) + "⁊"
-        else:
-            encoded += s[-1] + str(count)
-    else:
-        encoded += s[-1]
-    return encoded
+        i += 1
+
+    encoded_str += s[i - 1] + " " + str(count) + " "
+
+    return encoded_str
 
 def get_resolution(data):
     height = ""
@@ -71,8 +95,8 @@ def bmp_to_bit(path):
     bit_string = ""
     for pixel in pixels:
         bit_string += "1" if pixel == 255 else "0"
-    char = bit_string[-1]
-    while bit_string[len(bit_string)-1] == char:
+    chard = bit_string[-1]
+    while bit_string[len(bit_string)-1] == chard:
         bit_string=bit_string[:-1]
     return bit_string
 
@@ -94,16 +118,17 @@ def filewritting_encodedstr():
             content = file.read()
         encoded_string = txtencode(content)
     elif extension[-1] == "bmp":
-        bit_string = bmp_to_bit(file_path)
-        encoded_string = txtencode(bit_string)
         image = Image.open(file_path)
+        image = image.convert("RGB")
+        pixels = list(image.getdata())
+        encoded_string = bmp_encode(pixels)
         width, height = image.size
         encoded_string += "|"+ str(width) + "|"+ str(height) + "|"
 
     file_path = filedialog.asksaveasfilename(
-        filetypes=(("TXT files", "*.txt"),
+        filetypes=(("RLE files", "*.rle"),
                    ("HTML files", "*.html;*.htm"),
-                   ("RLE files", "*.rle"),
+                   ("TXT files", "*.txt"),
                    ("All files", "*.*")),
         defaultextension=".rle"
     )
@@ -129,8 +154,9 @@ def filewritting_decodedstr():
         width, height, content = get_resolution(content)
         width = int(width)
         height = int(height)
-        decoded_string = txtdecode(content)
-        image = bit_to_bmp(decoded_string,height,width)
+        pixels = bmp_decode(content)
+        image = Image.new("RGB", (width, height))
+        image.putdata(pixels)
         image.save(file_path)
     else:
         decoded_string = txtdecode(content)
@@ -144,7 +170,7 @@ root = customtkinter.CTk()
 root.geometry("400x500+100+200")
 root.title("Архиватор RLE 0.1")
 
-labelencode = customtkinter.CTkLabel(master=root, text="ENCODE BMP OR TXT FILE")  # Указываем родителя
+labelencode = customtkinter.CTkLabel(master=root, text="ENCODE BMP OR TXT FILE")
 labelencode.pack()
 
 compressbutton = customtkinter.CTkButton(master=root, text="Choose file", command=filewritting_encodedstr)
@@ -155,6 +181,5 @@ labeldecode.pack()
 
 decompressbutton = customtkinter.CTkButton(master=root, text="Choose file", command=filewritting_decodedstr)
 decompressbutton.pack()
-path_icon = "Main_icon.png"
-root.iconbitmap(path_icon)
+
 root.mainloop()
